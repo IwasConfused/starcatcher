352a18bd5908064122254402e68dc790aeeb116b
wd <lucasalvesbispo2@gmail.com>
2025-12-06 04:17:54 +0000

    improved mod compatibility with bucketed fishes and entities

--- src/main/java/com/wdiscute/starcatcher/registry/fishing/compatiblemods/EnvironmentalFishingProperties.java
+++ src/main/java/com/wdiscute/starcatcher/registry/fishing/compatiblemods/EnvironmentalFishingProperties.java
@@ -1,5 +1,6 @@
 package com.wdiscute.starcatcher.registry.fishing.compatiblemods;
 
+import com.wdiscute.starcatcher.StarcatcherTags;
 import com.wdiscute.starcatcher.registry.fishing.FishingPropertiesRegistry;
 import com.wdiscute.starcatcher.storage.FishProperties;
 
@@ -8,7 +9,6 @@ public class EnvironmentalFishingProperties extends FishingPropertiesRegistry
     public static void bootstrap()
     {
 
-        //region Environmental
         //
         //,------.                     ,--.                                                        ,--.            ,--.
         //|  .---' ,--,--,  ,--.  ,--. `--' ,--.--.  ,---.  ,--,--,  ,--,--,--.  ,---.  ,--,--,  ,-'  '-.  ,--,--. |  |
@@ -18,14 +18,26 @@ public class EnvironmentalFishingProperties extends FishingPropertiesRegistry
         //
 
         register(fish(fromRL("environmental", "koi"))
+                .withBucketedFish(fromRL("environmental", "koi_bucket"))
+                .withEntityToSpawn(rl("environmental", "koi"))
                 .withSizeAndWeight(FishProperties.sw(60, 20, 3000, 2000, 10, 20))
-                .withDifficulty(FishProperties.Difficulty.EVERYTHING_VANISHING)
+                .withDifficulty(FishProperties.Difficulty.EASY_FAST_FISH)
                 .withRarity(FishProperties.Rarity.UNCOMMON)
                 .withWorldRestrictions(FishProperties.WorldRestrictions.OVERWORLD
-                        .withMustBeCaughtAboveY(55)
                         .withBiomes(rl("environmental", "blossom_woods"), rl("environmental", "blossom_valleys")))
         );
 
-        //endregion Environmental
+        register(fish(fromRL("environmental", "slabfish_bucket"))
+                .withAlwaysSpawnEntity(true)
+                .withBucketedFish(fromRL("environmental", "slabfish_bucket"))
+                .withEntityToSpawn(rl("environmental", "slabfish_bucket"))
+                .withSizeAndWeight(FishProperties.sw(120, 40, 20000, 10000, 10, 20))
+                .withDifficulty(FishProperties.Difficulty.HARD)
+                .withRarity(FishProperties.Rarity.UNCOMMON)
+                .withWorldRestrictions(FishProperties.WorldRestrictions.OVERWORLD
+                        .withBiomes(rl("environmental", "marsh"))
+                        .withBiomesTags(StarcatcherTags.IS_SWAMP))
+        );
+
     }
 }
