2855c7a7a390566845a11a58d6bb017d0e229d6b
wd <lucasalvesbispo2@gmail.com>
2025-12-07 19:39:49 +0000

    fixed worm bait not fitting the rest of the item fished angle and speed

--- src/main/java/com/wdiscute/starcatcher/bob/FishingBobEntity.java
+++ src/main/java/com/wdiscute/starcatcher/bob/FishingBobEntity.java
@@ -50,7 +50,8 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-public class FishingBobEntity extends Projectile {
+public class FishingBobEntity extends Projectile
+{
     private static final Logger log = LoggerFactory.getLogger(FishingBobEntity.class);
 
 
@@ -75,19 +76,22 @@ public class FishingBobEntity extends Projectile {
 
     int ticksInFluid;
 
-    enum FishHookState {
+    enum FishHookState
+    {
         FLYING,
         BOBBING,
         BITING,
         FISHING
     }
 
-    public FishingBobEntity(EntityType<? extends FishingBobEntity> entityType, Level level) {
+    public FishingBobEntity(EntityType<? extends FishingBobEntity> entityType, Level level)
+    {
         super(entityType, level);
         player = null;
     }
 
-    public FishingBobEntity(Level level, Player player, ItemStack rod) {
+    public FishingBobEntity(Level level, Player player, ItemStack rod)
+    {
         super(ModEntities.FISHING_BOB.get(), level);
 
         this.player = player;
@@ -135,7 +139,8 @@ public class FishingBobEntity extends Projectile {
         currentState = FishHookState.FLYING;
     }
 
-    public void reel() {
+    public void reel()
+    {
         //server only
         List<FishProperties> available = new ArrayList<>(List.of());
 
@@ -153,7 +158,8 @@ public class FishingBobEntity extends Projectile {
                 FishProperties.Rarity.LEGENDARY, TrophyProperties.RarityProgress.DEFAULT
         ));
 
-        for (FishCaughtCounter fcc : player.getData(ModDataAttachments.FISHES_CAUGHT)) {
+        for (FishCaughtCounter fcc : player.getData(ModDataAttachments.FISHES_CAUGHT))
+        {
             all = new TrophyProperties.RarityProgress(all.total() + fcc.count(), all.unique());
 
             progressMap.compute(U.getFpFromRl(level(), fcc.fp()).rarity(), (r, p) -> new TrophyProperties.RarityProgress(p.total() + fcc.count(), p.unique() + 1));
@@ -162,9 +168,11 @@ public class FishingBobEntity extends Projectile {
 
         //check if any trophy can be caught
         e:
-        for (TrophyProperties tp : level().registryAccess().registryOrThrow(Starcatcher.TROPHY_REGISTRY)) {
+        for (TrophyProperties tp : level().registryAccess().registryOrThrow(Starcatcher.TROPHY_REGISTRY))
+        {
             //if tp can be caught
-            for (FishProperties.Rarity value : FishProperties.Rarity.values()) {
+            for (FishProperties.Rarity value : FishProperties.Rarity.values())
+            {
                 if (!check(progressMap.get(value), tp.getProgress(value))) continue e;
             }
 
@@ -172,7 +180,8 @@ public class FishingBobEntity extends Projectile {
                     && !trophiesCaught.contains(tp)
                     && FishProperties.getChance(tp.fp(), this, new ItemStack(ModItems.ROD.get())) > 0
                     && random.nextIntBetweenInclusive(0, 99) < tp.chanceToCatch()
-            ) {
+            )
+            {
 
                 ItemStack is = new ItemStack(tp.fish().value());
 
@@ -198,7 +207,8 @@ public class FishingBobEntity extends Projectile {
         }
 
         //if the treasure hook is equipped, drop vanilla treasure loot
-        if (bait.is(ModItems.WORM)) {
+        if (bait.is(ModItems.WORM))
+        {
             LootParams lootparams = new LootParams.Builder((ServerLevel) this.level())
                     .withParameter(LootContextParams.ORIGIN, this.position())
                     .withParameter(LootContextParams.TOOL, this.rod)
@@ -210,69 +220,71 @@ public class FishingBobEntity extends Projectile {
             LootTable table = level().getServer().reloadableRegistries().getLootTable(BuiltInLootTables.FISHING);
             List<ItemStack> items = table.getRandomItems(lootparams);
 
-            Vec3 rPos = player.position().subtract(this.position());
+            if(items.isEmpty()) return;
 
-            double rX = Math.clamp(rPos.x / 25, -1, 1) * 2.5;
-            double rY = Math.clamp(rPos.y / 20, -1, 1) * 2.5;
-            double rZ = Math.clamp(rPos.z / 25, -1, 1) * 2.5;
+            //make ItemEntity for item stack
+            ItemEntity itemFished = new ItemEntity(level(), position().x, position().y + 1.2f, position().z, items.get(0));
 
-            items.forEach(stack -> {
-                ItemEntity entity = new ItemEntity(level(), this.getX(), this.getY() + 1.2, this.getZ(), stack);
-                entity.setDeltaMovement(rX, rY, rZ);
-                level().addFreshEntity(entity);
-            });
+            //assign delta movement so fish flies towards player
+            double x = Math.clamp((player.position().x - position().x) / 25, -1, 1);
+            double y = Math.clamp((player.position().y - position().y) / 20, -1, 1);
+            double z = Math.clamp((player.position().z - position().z) / 25, -1, 1);
+            Vec3 vec3 = new Vec3(x, 0.7 + y, z);
+            itemFished.setDeltaMovement(vec3);
+
+            //add item entity to level
+            level().addFreshEntity(itemFished);
             this.kill();
             return;
         }
 
         //if no trophy is available, get chances of getting each fish
-        for (FishProperties fp : level().registryAccess().registryOrThrow(Starcatcher.FISH_REGISTRY)) {
+        for (FishProperties fp : level().registryAccess().registryOrThrow(Starcatcher.FISH_REGISTRY))
+        {
             int chance = FishProperties.getChance(fp, this, rod);
 
-            for (int i = 0; i < chance; i++) {
+            for (int i = 0; i < chance; i++)
+            {
                 available.add(fp);
             }
         }
 
         //if no fish is available, reset player fishing data and award nothing
-        if (available.isEmpty()) {
+        if (available.isEmpty())
+        {
             this.kill();
         }
 
-        if(bait.is(ModItems.SEEKING_WORM))
+        //get random fish from available pool
+        fpToFish = available.get(random.nextInt(available.size()));
+
+        //get uncaught fish if using seeking worm
+        if (bait.is(ModItems.SEEKING_WORM))
         {
             List<FishCaughtCounter> fishesCaught = player.getData(ModDataAttachments.FISHES_CAUGHT);
-            //get uncaught fish if using seeking worm
             for (FishProperties fp : available)
             {
                 boolean caught = false;
                 for (FishCaughtCounter fcc : fishesCaught)
-                {
-                    if(fp.equals(level().registryAccess().registryOrThrow(Starcatcher.FISH_REGISTRY).get(fcc.fp())))
-                    {
+                    if (fp.equals(level().registryAccess().registryOrThrow(Starcatcher.FISH_REGISTRY).get(fcc.fp())))
                         caught = true;
-                    }
-                }
-                if(!caught)
+
+                if (!caught)
                 {
                     fpToFish = fp;
                     break;
                 }
             }
-
-            fpToFish = available.get(random.nextInt(available.size()));
-        }
-        else
-        {
-            //get random fish from available pool
-            fpToFish = available.get(random.nextInt(available.size()));
         }
 
 
         //if skips minigame or server config of minigame enabled = false
-        if (fpToFish.skipMinigame() || !Config.ENABLE_MINIGAME.get()) {
+        if (fpToFish.skipMinigame() || !Config.ENABLE_MINIGAME.get())
+        {
             U.spawnFishFromFP(player, 0, false, false, 0);
-        } else {
+        }
+        else
+        {
             //otherwise send fishing minigame payload to client
             PacketDistributor.sendToPlayer(
                     ((ServerPlayer) player),
@@ -281,14 +293,17 @@ public class FishingBobEntity extends Projectile {
         }
 
         //consume bait
-        if (fpToFish.br().consumesBait()) {
-            if (fpToFish.catchInfo().bucketedFish().is(ModItems.MISSINGNO.getKey()) && bait.is(Items.BUCKET)) {
+        if (fpToFish.br().consumesBait())
+        {
+            if (!bait.is(Items.BUCKET))
+            {
                 bait.shrink(1);
                 rod.set(ModDataComponents.BAIT, new SingleStackContainer(bait));
                 return;
             }
 
-            if (!bait.is(Items.BUCKET)) {
+            if (bait.is(Items.BUCKET) && !fpToFish.catchInfo().bucketedFish().is(ModItems.MISSINGNO.getKey()))
+            {
                 bait.shrink(1);
                 rod.set(ModDataComponents.BAIT, new SingleStackContainer(bait));
             }
@@ -296,49 +311,61 @@ public class FishingBobEntity extends Projectile {
         }
     }
 
-    private boolean shouldStopFishing(Player player) {
+    private boolean shouldStopFishing(Player player)
+    {
         if (level().isClientSide) return false;
 
         boolean holdingRod = player.getMainHandItem().is(StarcatcherTags.RODS)
                 || player.getOffhandItem().is(StarcatcherTags.RODS);
 
-        if (!player.isRemoved() && player.isAlive() && holdingRod && !(this.distanceToSqr(player) > 1024)) {
+        if (!player.isRemoved() && player.isAlive() && holdingRod && !(this.distanceToSqr(player) > 1024))
+        {
             return false;
-        } else {
+        }
+        else
+        {
             this.kill();
             return true;
         }
     }
 
     @Override
-    public boolean fireImmune() {
+    public boolean fireImmune()
+    {
         return netherite_upgraded;
     }
 
     @Override
-    public void lavaHurt() {
+    public void lavaHurt()
+    {
         super.lavaHurt();
-        if (!netherite_upgraded && !level().isClientSide) {
+        if (!netherite_upgraded && !level().isClientSide)
+        {
             kill();
         }
     }
 
     @Override
-    public void kill() {
+    public void kill()
+    {
         player.setData(ModDataAttachments.FISHING, "");
         super.kill();
     }
 
     @Override
-    public void tick() {
+    public void tick()
+    {
         super.tick();
 
-        if (!level().isClientSide) {
+        if (!level().isClientSide)
+        {
             if (currentState == FishHookState.FLYING) entityData.set(STATE, 1);
             if (currentState == FishHookState.BOBBING) entityData.set(STATE, 2);
             if (currentState == FishHookState.BITING) entityData.set(STATE, 3);
             if (currentState == FishHookState.FISHING) entityData.set(STATE, 4);
-        } else {
+        }
+        else
+        {
             if (entityData.get(STATE) == 1) currentState = FishHookState.FLYING;
             if (entityData.get(STATE) == 2) currentState = FishHookState.BOBBING;
             if (entityData.get(STATE) == 3) currentState = FishHookState.BITING;
@@ -346,7 +373,8 @@ public class FishingBobEntity extends Projectile {
         }
 
         Player player = ((Player) this.getOwner());
-        if (player == null || this.shouldStopFishing(player)) {
+        if (player == null || this.shouldStopFishing(player))
+        {
             this.discard();
             if (player != null) player.setData(ModDataAttachments.FISHING.get(), "");
         }
@@ -355,20 +383,24 @@ public class FishingBobEntity extends Projectile {
         FluidState fluid = this.level().getFluidState(blockpos);
         FluidState fluidBellow = this.level().getFluidState(blockpos.below());
 
-        if (this.currentState == FishHookState.FLYING) {
+        if (this.currentState == FishHookState.FLYING)
+        {
             if (getDeltaMovement().y < 1.2f)
                 this.setDeltaMovement(this.getDeltaMovement().add(0, -0.02, 0));
 
-            if (!fluid.isEmpty()) {
+            if (!fluid.isEmpty())
+            {
                 this.setDeltaMovement(this.getDeltaMovement().multiply(0.3, 0.3, 0.3));
                 if (!level().isClientSide) this.currentState = FishHookState.BOBBING;
                 return;
             }
         }
 
-        if (this.currentState == FishHookState.BITING) {
+        if (this.currentState == FishHookState.BITING)
+        {
             timeBiting++;
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 5; i++)
+            {
                 if (level().getFluidState(blockpos).is(Fluids.LAVA))
                     level().addParticle(
                             ModParticles.FISHING_BITING_LAVA.get(),
@@ -385,29 +417,40 @@ public class FishingBobEntity extends Projectile {
                             0, 0, 0);
             }
 
-            if (timeBiting > 80) {
+            if (timeBiting > 80)
+            {
                 player.setData(ModDataAttachments.FISHING, "");
                 kill();
             }
-        } else {
+        }
+        else
+        {
             timeBiting = 0;
         }
 
         //if theres no fluid on block or under, changes to FLYING
-        if (fluid.isEmpty() && fluidBellow.isEmpty()) {
+        if (fluid.isEmpty() && fluidBellow.isEmpty())
+        {
             if (!level().isClientSide) currentState = FishHookState.FLYING;
         }
 
         //TODO check for water level instead of just blockstate to make the entity sit better in water
-        if (this.currentState == FishHookState.BOBBING || this.currentState == FishHookState.FISHING) {
+        if (this.currentState == FishHookState.BOBBING || this.currentState == FishHookState.FISHING)
+        {
             checkForFish();
 
-            if (!fluid.isEmpty()) {
+            if (!fluid.isEmpty())
+            {
                 setDeltaMovement(this.getDeltaMovement().add(0.0F, 0.01, 0.0F));
-            } else {
-                if (random.nextFloat() > 0.02) {
+            }
+            else
+            {
+                if (random.nextFloat() > 0.02)
+                {
                     setDeltaMovement(this.getDeltaMovement().add(0.0F, -0.03, 0.0F));
-                } else {
+                }
+                else
+                {
                     setDeltaMovement(this.getDeltaMovement().add(0.0F, -0.01, 0.0F));
                 }
             }
@@ -416,7 +459,8 @@ public class FishingBobEntity extends Projectile {
         this.move(MoverType.SELF, this.getDeltaMovement());
         //this.updateRotation();
 
-        if (this.onGround() || this.horizontalCollision) {
+        if (this.onGround() || this.horizontalCollision)
+        {
             this.setDeltaMovement(Vec3.ZERO);
         }
 
@@ -424,22 +468,29 @@ public class FishingBobEntity extends Projectile {
         this.reapplyPosition();
     }
 
-    public boolean checkBiting() {
+    public boolean checkBiting()
+    {
 
-        if (currentState == FishHookState.BITING) {
+        if (currentState == FishHookState.BITING)
+        {
             currentState = FishHookState.FISHING;
             reel();
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
 
-    private void checkForFish() {
-        if (!level().isClientSide && currentState == FishHookState.BOBBING) {
+    private void checkForFish()
+    {
+        if (!level().isClientSide && currentState == FishHookState.BOBBING)
+        {
             ticksInFluid++;
             int i = random.nextInt(chanceToFishEachTick);
-            if ((i == 1 || ticksInFluid > maxTicksToFish) && ticksInFluid > minTicksToFish) {
+            if ((i == 1 || ticksInFluid > maxTicksToFish) && ticksInFluid > minTicksToFish)
+            {
                 if (Config.SHOW_EXCLAMATION_MARK_PARTICLE.get())
                     ((ServerLevel) level()).sendParticles(
                             ModParticles.FISHING_NOTIFICATION.get(),
@@ -456,17 +507,20 @@ public class FishingBobEntity extends Projectile {
     }
 
     @Override
-    public AABB getBoundingBoxForCulling() {
+    public AABB getBoundingBoxForCulling()
+    {
         AABB box = new AABB(-10, -10, -10, 10, 10, 10);
         return box.move(position());
     }
 
     @Override
-    protected void defineSynchedData(SynchedEntityData.Builder builder) {
+    protected void defineSynchedData(SynchedEntityData.Builder builder)
+    {
         builder.define(STATE, 0);
     }
 
-    public static boolean check(TrophyProperties.RarityProgress current, TrophyProperties.RarityProgress restriction) {
+    public static boolean check(TrophyProperties.RarityProgress current, TrophyProperties.RarityProgress restriction)
+    {
         return current.total() >= restriction.total() && current.unique() >= restriction.unique();
     }
 
