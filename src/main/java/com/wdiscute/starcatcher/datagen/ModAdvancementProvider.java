58a121edaccc0114d4aea4029db0878c896dbab4
wd <lucasalvesbispo2@gmail.com>
2025-12-15 18:29:44 +0000

    made trash have super easy minigame instead of skipping, removed advancement

--- src/main/java/com/wdiscute/starcatcher/datagen/ModAdvancementProvider.java
+++ src/main/java/com/wdiscute/starcatcher/datagen/ModAdvancementProvider.java
@@ -1,15 +1,8 @@
 package com.wdiscute.starcatcher.datagen;
 
-import com.wdiscute.starcatcher.Starcatcher;
-import com.wdiscute.starcatcher.registry.ModCriterionTriggers;
-import com.wdiscute.starcatcher.registry.ModItems;
-import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
-import net.minecraft.advancements.AdvancementType;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
@@ -27,23 +20,23 @@ public class ModAdvancementProvider extends AdvancementProvider {
         @SuppressWarnings("removal")
         @Override
         public void generate(HolderLookup.Provider provider, Consumer<AdvancementHolder> consumer, ExistingFileHelper existingFileHelper) {
-            consumer.accept(Advancement.Builder
-                    .advancement()
-                    .display(
-                            ModItems.AURORA.get(),
-                            Component.translatable("advancements.husbandry.starcatcher.fisherman.title"),
-                            Component.translatable("advancements.husbandry.starcatcher.fisherman.description"),
-                            null,
-                            AdvancementType.CHALLENGE,
-                            true,
-                            true,
-                            false
-                    ).addCriterion("perfect_catch", ModCriterionTriggers.MINIGAME_COMPLETED.get()
-                            .builder()
-                            .perfect()
-                            .build()
-                    ).parent(ResourceLocation.withDefaultNamespace("husbandry/fishy_business")).build(Starcatcher.rl("husbandry/fisherman"))
-            );
+//            consumer.accept(Advancement.Builder
+//                    .advancement()
+//                    .display(
+//                            ModItems.AURORA.get(),
+//                            Component.translatable("advancements.husbandry.starcatcher.fisherman.title"),
+//                            Component.translatable("advancements.husbandry.starcatcher.fisherman.description"),
+//                            null,
+//                            AdvancementType.CHALLENGE,
+//                            true,
+//                            true,
+//                            false
+//                    ).addCriterion("perfect_catch", ModCriterionTriggers.MINIGAME_COMPLETED.get()
+//                            .builder()
+//                            .perfect()
+//                            .build()
+//                    ).parent(ResourceLocation.withDefaultNamespace("husbandry/fishy_business")).build(Starcatcher.rl("husbandry/fisherman"))
+//            );
         }
     }
 }
